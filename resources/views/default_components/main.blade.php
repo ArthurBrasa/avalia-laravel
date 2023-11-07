2ddce496bfe80526ec8485948cea8b6f9c643a54
Arthur Brasa <arthurbrasa@outlook.com>
2023-11-07 18:07:57 -0300

    home

---
+++ resources/views/default_components/main.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Avalia - UnB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="css/custom.css">
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+    <!-- Liveware -->
+    @livewireStyles
+</head>
+  <body class="bg">
+
+    <!-- Navbar section -->
+    @include('default_components.navbar')
+
+    <div class="main-content">
+        @yield('content')
+    </div>
+    
+    @livewireScripts
+  
+  </body>
+</html>
---
+++ resources/views/default_components/navbar.blade.php
@@ -0,0 +1,34 @@
+<header class="">
+    <nav class="navbar navbar-expand-lg px-5" aria-label="Third navbar example">
+        <div class="container-fluid">
+          <a class="navbar-brand logo fs-1 fw-bold text-primary" href="#">Avalia UnB</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse" id="navbarsExample03">
+            <ul class="navbar-nav m-auto mb-2 mb-sm-0 fs-4 gap-3">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Dashboard</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Departamento</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link "  href="#" aria-disabled="true">Avaliações</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link "  href="#" aria-disabled="true">Sobre Nós</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn bg-success"  href="{{ route('login') }}" aria-disabled="true">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link btn bg-success "  href="{{ route('register') }}" aria-disabled="true">Registrar</a>
+              </li>
+            </ul>
+          </div >
+
+        </div>
+      </nav>
+  </header>
