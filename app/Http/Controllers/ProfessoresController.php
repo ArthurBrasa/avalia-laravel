215a247d372f7cafded9a6ec278c2faf56b77272
Arthur Brasa <arthurbrasa@outlook.com>
2023-12-03 16:24:01 -0300

    listaprofessores

--- app/Http/Controllers/ProfessoresController.php
+++ app/Http/Controllers/ProfessoresController.php
@@ -12,7 +12,7 @@ class ProfessoresController extends Controller
      */
     public function index()
     {
-        return view('system.professores');
+        return view('system.listaProfessores');
     }
 
    
