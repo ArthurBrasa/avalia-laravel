4ef12f01dbc848f1984d22adcacc0522c8c8776c
Arthur Brasa <arthurbrasa@outlook.com>
2023-12-03 17:51:21 -0300

    upper case

--- app/Http/Controllers/InfoMateriasController.php
+++ app/Http/Controllers/InfoMateriasController.php
@@ -9,7 +9,7 @@ class InfoMateriasController extends Controller
 {
     //
     public function index(Request $request, $nome){
-        $nome = strtoupper(str_replace('-', ' ', $nome));
+        $nome = ucwords(str_replace('-', ' ', $nome));
 
         return view('system.InfoMateria', ['nome' => $nome]);
     }
