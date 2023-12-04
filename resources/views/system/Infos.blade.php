76f16e1271d7d6f3acc51e20cf7b9717e4bb1254
Arthur Brasa <arthurbrasa@outlook.com>
2023-12-04 09:41:08 -0300

    asset

--- resources/views/system/Infos.blade.php
+++ resources/views/system/Infos.blade.php
@@ -15,7 +15,7 @@
         <!--  -->
         <div class="d-flex flex-column flex-md-row gap-5">
           <div class="card shadow-sm p-3 mb-5 bg-body rounded">
-            <img class="card-img p-3" width="300px" src='{{asset("imgs/svg/Proffs/$img.svg")}}' alt="">
+            <img class="card-img p-3" width="300px" src='{{asset("imgs/svg/Proffs/$img.svg", true)}}' alt="">
             <p class="card-body">{{$nome}}</p>
           </div>
   
