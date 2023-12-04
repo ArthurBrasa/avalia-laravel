b0938875de94d8f3188d867a92c90cca642d4e80
Arthur Brasa <arthurbrasa@outlook.com>
2023-12-04 09:46:49 -0300

    svg

--- resources/views/components/professor-card.blade.php
+++ resources/views/components/professor-card.blade.php
@@ -1,6 +1,6 @@
 <div class="col">
   <div class="card shadow-sm">
-      <img class="p-3" src='{{ asset("imgs/svg/Proffs/" . ($img ?? "default") . ".svg", true) }}' alt="">
+      <img class="p-3" src='{{ asset("imgs/svg/Proffs/$img.svg", true) }}' alt="">
       <div class="card-body">
           <p class="card-text">{{ $nome ?? "Unkown" }}</p>
           <div class="d-flex justify-content-between align-items-center">
