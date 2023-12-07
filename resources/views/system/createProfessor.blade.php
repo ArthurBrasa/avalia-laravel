9432f6abd39dd4e151567fb6bad7e63622d64124
Arthur Brasa <arthurbrasa@outlook.com>
2023-12-07 00:19:02 -0300

    commit

--- resources/views/system/createProfessor.blade.php
+++ resources/views/system/createProfessor.blade.php
@@ -160,7 +160,7 @@
     <!-- Button trigger modal -->
 
   
-  <!-- Modal -->
+  {{-- <!-- Modal -->
   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -179,6 +179,6 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
 
 @endsection
